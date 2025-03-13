ca3cfbf73acaf5ed05fd80e53ce50bc47e7cdcfe
Mauricio Garcia <mauricio@solverank.tech>
2025-03-13 14:40:47 -0300

    .

--- intro.py
+++ intro.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 'Under construction'
 
+st.set_page_config(page_title='Python Tutorial', page_icon=':books:', layout='wide')
+
 st.image('https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif', width=200)
