bb1061f0f75b7ce7e06a8f2dd0616ed55666c474
Mauricio Garcia <mauricio@solverank.tech>
2025-03-13 23:13:02 -0300

    .

--- intro.py
+++ intro.py
@@ -7,3 +7,8 @@ st.write('This is a tutorial on the main Python commands.')
 st.write('It is not a course, just a quick reference of the main commands.')
 st.write('If you want a Python book for beginners, check out [Solverbooks](https://solvertank.tech/solverbooks).')
 st.write('If you want to practice the commands, use [Solverpills](https://solvertank.tech/solverpills).')
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
+st.write('<a href="https://www.solvertank.tech"><img src="https://www.solvertank.tech/images/cube.png" width="75"></a>', unsafe_allow_html=True)
