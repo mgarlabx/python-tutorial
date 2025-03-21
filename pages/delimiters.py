057521a1bf1adb985d80750e882462ed87ca7c87
Mauricio Garcia <mauricio@solverank.tech>
2025-03-21 18:34:26 -0300

    .

---
+++ pages/delimiters.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+
+data = [
+    ['[]','brackets','colchetes','corchetes'],
+    ['()','parentheses','parênteses','paréntesis'],
+    ['{}','braces','chaves','llaves'],
+    [',','comma','vírgula','coma'],
+    [';','semicolon','ponto e vírgula','punto y coma'],
+    [':','colon','dois pontos','dos puntos'],
+    ['.','dot','ponto','punto'],
+    ['"','double quotes','aspas','comillas dobles'],
+    ["'",'single quotes','aspas simples','comillas simples'],
+    ['-','dash','traço','guión'],
+    ['_','underscore','traço baixo','guión bajo'],
+    ['/','slash','barra','barra'],
+    ['\\','backslash','barra invertida','barra invertida'],
+    ['#','hash','cerquilha','almohadilla / numeral'],
+    ['B','bold','negrito','negrita'],
+    ['U','underlined','sublinhado','subrayado'],
+    ['I','italic','itálico','itálica'],
+    ['!','exclamation mark','ponto de exclamação','signo de exclamación'],
+    ['?','question mark','ponto de interrogação','signo de interrogación'],
+]
+
+df = pd.DataFrame(data, columns=['Symbol','English','Portuguese','Spanish'])
+
+st.dataframe(df, height=710)
